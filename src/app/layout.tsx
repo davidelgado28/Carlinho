2c646a72113e36d505adb8508518d6ff3a14b16d
David Carlos <davidcarlosdelgado28@gmail.com>
2026-09-15 19:29:54 -0300

    Add metadata and root layout to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "Carlinho OS",
+  description: "Web-based OS for Software Development",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
